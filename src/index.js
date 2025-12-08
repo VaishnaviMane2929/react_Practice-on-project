f3010f79b02540c77acb20a705f161ab5b18df8f
vaishnavi <vaishnavimane991@gmail.com>
2025-12-08 23:19:01 +0530

    custom react adds

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from 
+
+const root  = ReactDom.creatRoot(document.getElementById 
+    ('root'));
+root.render (
+    <React.StrictMode >
+     <App />
+    <React.StrictMode />
+
+);
