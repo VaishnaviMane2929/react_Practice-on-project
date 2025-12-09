743b818bdc3caf0e3b1b9f8b93dbfbab1f1b7b77
vaishnavi <vaishnavimane991@gmail.com>
2025-12-09 23:31:53 +0530

    custom react adds

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,42 @@
-import { StrictMode } from 'react'
+import React, { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Demo from './demo.jsx'
 
-createRoot(document.getElementById('root')).render(
+function MyApp() {
+    return(
+        <div>
+            <h1>this is custom react</h1>
+        </div>
+    )
+
+}
+
+const ReactElement= {
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        terget: '_blank'
+
+    },
+    Children:' hello this use the custom react '
+}
+
+const anotherElement=(
+    <a href="https://google.com"> visit google</a>
+
+)
+
+const reactElement=  React.createElement(
+    'a',
+    {href:'https://google.com',terget: '_blank'}, 'click and visit the google'
+    
+)
+
+
+ReactDom.createRoot(document.getElementById('root')).render(
   
-    <App />,
-    <Demo />
+    reactElement
   
 )
